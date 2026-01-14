e6de50bc057a192ed4dbbf0acdce5fe6ed956db8
Nurettin <112160896+nurettindemirell@users.noreply.github.com>
2026-01-14 14:20:09 +0300

    Updated1.

--- CustomerPortal/src/main.jsx
+++ CustomerPortal/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
 import awsConfig from './aws-exports'
 import App from './App.jsx'
-import './index.css'
+import './index.css'            
+
 
 Amplify.configure(awsConfig);
 
